c7c3d158422cc97c35b2c4cf963bb5ec12a61072
zihan zhou <15645113830zzh@gmail.com>
2025-06-04 20:43:18 +0800

    kernel/sched/fair: Reduce the default slice to avoid tasks getting an extra tick
    The old default value for slice is 0.75 msec * (1 + ilog(ncpus)) which
    means that we have a default slice of:
    
      0.75 for 1 cpu
      1.50 up to 3 cpus
      2.25 up to 7 cpus
      3.00 for 8 cpus and above.
    
    For HZ=250 and HZ=100, because of the tick accuracy, the runtime of
    tasks is far higher than their slice.
    
    For HZ=1000 with 8 cpus or more, the accuracy of tick is already
    satisfactory, but there is still an issue that tasks will get an extra
    tick because the tick often arrives a little faster than expected. In
    this case, the task can only wait until the next tick to consider that it
    has reached its deadline, and will run 1ms longer.
    
    vruntime + sysctl_sched_base_slice =     deadline
            |-----------|-----------|-----------|-----------|
                 1ms          1ms         1ms         1ms
                       ^           ^           ^           ^
                     tick1       tick2       tick3       tick4(nearly 4ms)
    
    There are two reasons for tick error: clockevent precision and the
    CONFIG_IRQ_TIME_ACCOUNTING/CONFIG_PARAVIRT_TIME_ACCOUNTING. with
    CONFIG_IRQ_TIME_ACCOUNTING every tick will be less than 1ms, but even
    without it, because of clockevent precision, tick still often less than
    1ms.
    
    In order to make scheduling more precise, we changed 0.75 to 0.70,
    Using 0.70 instead of 0.75 should not change much for other configs
    and would fix this issue:
    
      0.70 for 1 cpu
      1.40 up to 3 cpus
      2.10 up to 7 cpus
      2.8 for 8 cpus and above.
    
    This does not guarantee that tasks can run the slice time accurately
    every time, but occasionally running an extra tick has little impact.
    
    Signed-off-by: zihan zhou <15645113830zzh@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20250208075322.13139-1-15645113830zzh@gmail.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -125,14 +125,14 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_L
  * Minimal preemption granularity for CPU-bound tasks:
  *
  * (BORE default: 3 msec constant, units: nanoseconds)
- * (CFS  default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (CFS  default: 0.70 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
 #ifdef CONFIG_SCHED_BORE
 unsigned int sysctl_sched_min_granularity			= 3000000ULL;
 unsigned int normalized_sysctl_sched_min_granularity	= 3000000ULL;
 #else // CONFIG_SCHED_BORE
-unsigned int sysctl_sched_min_granularity			= 750000ULL;
-unsigned int normalized_sysctl_sched_min_granularity	= 750000ULL;
+unsigned int sysctl_sched_min_granularity			= 700000ULL;
+unsigned int normalized_sysctl_sched_min_granularity	= 700000ULL;
 #endif // CONFIG_SCHED_BORE
 
 /*
